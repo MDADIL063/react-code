308c9e414274c08c50f0906821f68590c1c7c58f
Md Adil <khanmdadil063@gmail.com>
2024-02-13 12:20:54 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I Am a h1 Tag"),
+    React.createElement("h3", {}, "I Am a h3 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I Am a h1 Tag"),
+    React.createElement("h3", {}, "I Am a h3 Tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
